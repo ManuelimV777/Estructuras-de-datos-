772bb10841e765e494939e99f059a271c82d4f99
Victor Manuel Gonzalez Villaseca <230i0061@martineztorre.tecnm.mx>
2024-09-15 12:47:38 -0600

    Add files via upload

---
+++ arreglosdeArreglos .py
@@ -0,0 +1,7 @@
+
+numeros = [2, 5, [2, 6, [4, 2, "manu"], 7], 6, 4, 8]
+mostrar = numeros[2][2][2][0]  # Accede al elemento "manu" dentro de la lista anidada
+
+print(mostrar)  # Imprime "manu"
+print(len(numeros))  # Imprime la longitud de la lista "numeros" (6)
+print(type(numeros))  # Imprime el tipo de dato de la variable "numeros" (list)
